e3a29b87d63d293a571ef0e5722870da709a0f6d
이지훈 <easyhoon119@github.com>
2021-03-26 22:33:44 +0900

    second commit

---
+++ event.js
@@ -0,0 +1,10 @@
+const game_btn = document.querySelector('.game_btn');
+const quiz_btn = document.querySelector('.quiz_btn');
+
+game_btn.addEventListener('click', () => {
+    window.location.href = 'https://myongjiblossom-card.netlify.app/';
+});
+
+quiz_btn.addEventListener('click', () => {
+    window.location.href = 'https://forms.gle/xWJDoKyNgodAkR9N8';
+});
---
+++ main.js
@@ -0,0 +1,21 @@
+const face_btn = document.querySelector('.facebook_btn');
+const insta_btn = document.querySelector('.insta_btn');
+const community_btn = document.querySelector('.community_btn');
+const event_btn = document.querySelector('.event_btn');
+
+face_btn.addEventListener('click', () => {
+    window.location.href = 'https://www.facebook.com/MJUy48/';
+});
+
+insta_btn.addEventListener('click', () => {
+    window.location.href = 'https://www.instagram.com/mju_run/'
+});
+
+community_btn.addEventListener('click', () => {
+    window.location.href = 'http://pf.kakao.com/_fUCpK';
+});
+
+event_btn.addEventListener('click', () => {
+    window.location.href = '/event.html';
+});
+

d051bf29fba261ed7b01871bfd6bf4eef43b1d67
이지훈 <easyhoon119@github.com>
2021-04-06 10:58:07 +0900

    change js

--- event.js
+++ event.js
@@ -1,9 +1,7 @@
-const game_btn = document.querySelector('.game_btn');
+
 const quiz_btn = document.querySelector('.quiz_btn');
 
-game_btn.addEventListener('click', () => {
-    window.location.href = 'https://myongjiblossom-card.netlify.app/';
-});
+
 
 quiz_btn.addEventListener('click', () => {
     window.location.href = 'https://forms.gle/xWJDoKyNgodAkR9N8';

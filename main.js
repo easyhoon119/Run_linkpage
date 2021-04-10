d83a63596ec6922a3e93b590b04e1bf5a397d7b8
이지훈 <easyhoon119@github.com>
2021-04-11 08:33:27 +0900

    delete event.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const face_btn = document.querySelector('.facebook_btn');
 const insta_btn = document.querySelector('.insta_btn');
 const community_btn = document.querySelector('.community_btn');
-const event_btn = document.querySelector('.event_btn');
+// const event_btn = document.querySelector('.event_btn');
 
 face_btn.addEventListener('click', () => {
     window.location.href = 'https://www.facebook.com/MJUy48/';
@@ -15,7 +15,7 @@ community_btn.addEventListener('click', () => {
     window.location.href = 'http://pf.kakao.com/_fUCpK';
 });
 
-event_btn.addEventListener('click', () => {
-    window.location.href = '/event.html';
-});
+// event_btn.addEventListener('click', () => {
+//     window.location.href = '/event.html';
+// });
 

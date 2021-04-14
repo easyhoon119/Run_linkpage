2e591b6382c15a0582a9bf15f51efb5f1ce39cf5
이지훈 <easyhoon119@github.com>
2021-04-14 13:23:39 +0900

    add sign

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const face_btn = document.querySelector('.facebook_btn');
 const insta_btn = document.querySelector('.insta_btn');
 const community_btn = document.querySelector('.community_btn');
-// const event_btn = document.querySelector('.event_btn');
+const download_btn = document.querySelector('.download_btn');
+const mail_btn = document.querySelector('.mail_btn');
+var email = 'mju_run@naver.com';
+var subject = '2021년도 Run 총학생회 서명운동 참여하기';
+var last_msg = '이곳에 서명문에 서명을 기입하여 첨부해주세요!';
 
 face_btn.addEventListener('click', () => {
     window.location.href = 'https://www.facebook.com/MJUy48/';
@@ -15,7 +19,11 @@ community_btn.addEventListener('click', () => {
     window.location.href = 'http://pf.kakao.com/_fUCpK';
 });
 
-// event_btn.addEventListener('click', () => {
-//     window.location.href = '/event.html';
-// });
+mail_btn.addEventListener('click', () => {
+    window.location.href = 'mailto:' + email + '?subject=' + subject + '&body=' + last_msg;
+});
+
+download_btn.addEventListener('click', () => {
+    window.location.href = 'downloadto:' + './kakao.png';
+});
 

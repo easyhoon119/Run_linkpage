0293712ca50217d2b4b13d473204ab6b3c719fd5
이지훈 <easyhoon119@github.com>
2021-10-18 17:25:39 +0900

    add voting

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ const community_btn = document.querySelector('.community_btn');
 const download_btn = document.querySelector('.download_btn');
 const mail_btn = document.querySelector('.mail_btn');
 const download_btn2 = document.querySelector('.download_btn2');
+const commission_btn = document.querySelector('.centerCommission');
+const num1_btn = document.querySelector('.voting_num1');
+const num2_btn = document.querySelector('.voting_num2');
 // const event_btn = document.querySelector('.event_btn');
 
 var email = 'mju_run@naver.com';
@@ -24,6 +27,18 @@ community_btn.addEventListener('click', () => {
 
 mail_btn.addEventListener('click', () => {
     window.location.href = 'mailto:' + email + '?subject=' + subject + '&body=' + last_msg;
+});
+
+commission_btn.addEventListener('click',()=>{
+    window.location.href = 'https://www.instagram.com/mju_election_commission/?hl=ko';
+});
+
+num1_btn.addEventListener('click',()=>{
+    window.location.href = 'https://www.instagram.com/mju_ec_jeong/?hl=ko';
+});
+
+num2_btn.addEventListener('click',()=>{
+    window.location.href = 'https://www.instagram.com/mju_ec_alt/?hl=ko';
 });
 
 // event_btn.addEventListener('click', () => {
